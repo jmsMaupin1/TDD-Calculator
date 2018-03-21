e6605ba1ab8b005b82f58cb744eb38674fe229ba
JmsMaupin1 <jmsMaupin1@yahoo.com>
2018-03-21 16:52:05 -0400

    Add the ability to press numbers and have them appear on screen, and clear the numbers. Adds tests as well

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'; 
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      currentNumber: "0",
+      currentOp: ""
+    }
+  }
+
+  handleNumberClick(n) {
+    this.setState({currentNumber: n})
+  }
+
+
+
   render() {
     return (
       <div className="App" style={{paddingTop: "5px"}}>
-        <div className="col-lg-3 clearfix" style={{margin: "auto", border: "1px solid black"}}>
-          <p style={{float: "right"}}></p>
-          <br/>
-          <br/>
-          <h2 style={{float: "right"}}>0</h2>
+        <div id="display" className="col-lg-3 clearfix" style={{margin: "auto", border: "1px solid black"}}>
+          <h2 style={{float: "right"}}>{this.state.currentNumber}</h2>
         </div>
 
         <div className="col-lg-3" style={{margin: "10px auto"}}>
@@ -22,31 +33,31 @@ class App extends Component {
           </div>
           <div className="row buttonpad">
             <button className="col-lg-2 col-md-2 col-sm-2 button">CE</button>
-            <button className="col-lg-2 col-md-2 col-sm-2 button">C</button>
+            <button onClick={()=>this.setState({currentNumber: "0"})} className="col-lg-2 col-md-2 col-sm-2 button">C</button>
             <button className="col-lg-2 col-md-2 col-sm-2 button">←</button>
             <button className="col-lg-2 col-md-2 col-sm-2 button">/</button>
           </div>
           <div className="row buttonpad">
-            <button className="col-lg-2 col-md-2 col-sm-2 button">7</button>
-            <button className="col-lg-2 col-md-2 col-sm-2 button">8</button>
-            <button className="col-lg-2 col-md-2 col-sm-2 button">9</button>
+            <button onClick={this.handleNumberClick.bind(this, 7)} className="col-lg-2 col-md-2 col-sm-2 button">7</button>
+            <button onClick={this.handleNumberClick.bind(this, 8)} className="col-lg-2 col-md-2 col-sm-2 button">8</button>
+            <button onClick={this.handleNumberClick.bind(this, 9)} className="col-lg-2 col-md-2 col-sm-2 button">9</button>
             <button className="col-lg-2 col-md-2 col-sm-2 button">X</button>
           </div>
           <div className="row buttonpad">
-            <button className="col-lg-2 col-md-2 col-sm-2 button">4</button>
-            <button className="col-lg-2 col-md-2 col-sm-2 button">5</button>
-            <button className="col-lg-2 col-md-2 col-sm-2 button">6</button>
+            <button onClick={this.handleNumberClick.bind(this, 4)} className="col-lg-2 col-md-2 col-sm-2 button">4</button>
+            <button onClick={this.handleNumberClick.bind(this, 5)} className="col-lg-2 col-md-2 col-sm-2 button">5</button>
+            <button onClick={this.handleNumberClick.bind(this, 6)} className="col-lg-2 col-md-2 col-sm-2 button">6</button>
             <button className="col-lg-2 col-md-2 col-sm-2 button">-</button>
           </div>
           <div className="row buttonpad">
-            <button className="col-lg-2 col-md-2 col-sm-2 button">1</button>
-            <button className="col-lg-2 col-md-2 col-sm-2 button">2</button>
-            <button className="col-lg-2 col-md-2 col-sm-2 button">3</button>
+            <button onClick={this.handleNumberClick.bind(this, 1)} className="col-lg-2 col-md-2 col-sm-2 button">1</button>
+            <button onClick={this.handleNumberClick.bind(this, 2)} className="col-lg-2 col-md-2 col-sm-2 button">2</button>
+            <button onClick={this.handleNumberClick.bind(this, 3)} className="col-lg-2 col-md-2 col-sm-2 button">3</button>
             <button className="col-lg-2 col-md-2 col-sm-2 button">+</button>
           </div>
           <div className="row buttonpad">
             <button className="col-lg-2 col-md-2 col-sm-2 button">+/-</button>
-            <button className="col-lg-2 col-md-2 col-sm-2 button">0</button>
+            <button onClick={this.handleNumberClick.bind(this, 0)} className="col-lg-2 col-md-2 col-sm-2 button">0</button>
             <button className="col-lg-2 col-md-2 col-sm-2 button">.</button>
             <button className="col-lg-2 col-md-2 col-sm-2 button">=</button>
           </div>
--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,121 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({adapter: new Adapter() });
+
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+describe('Renders a number when number is first thing pressed', () => {
+	it('Renders 1 when the 1 button is pressed', ()=>{
+		let wrapper = shallow(<App/>);
+		wrapper.find('button[children="1"]')
+			.simulate('click');
+
+		expect(wrapper.find('#display>h2[children=1]').length).toBe(1)
+	})
+
+	it('Renders 2 when the 2 button is pressed', ()=>{
+		let wrapper = shallow(<App/>);
+		wrapper.find('button[children="2"]')
+			.simulate('click');
+
+		expect(wrapper.find('#display>h2[children=2]').length).toBe(1)
+	})
+
+	it('Renders 3 when the 3 button is pressed', ()=>{
+		let wrapper = shallow(<App/>);
+		wrapper.find('button[children="3"]')
+			.simulate('click');
+
+		expect(wrapper.find('#display>h2[children=3]').length).toBe(1)
+	})
+
+	it('Renders 4 when the 4 button is pressed', ()=>{
+		let wrapper = shallow(<App/>);
+		wrapper.find('button[children="4"]')
+			.simulate('click');
+
+		expect(wrapper.find('#display>h2[children=4]').length).toBe(1)
+	})
+
+	it('Renders 5 when the 5 button is pressed', ()=>{
+		let wrapper = shallow(<App/>);
+		wrapper.find('button[children="5"]')
+			.simulate('click');
+
+		expect(wrapper.find('#display>h2[children=5]').length).toBe(1)
+	})
+
+	it('Renders 6 when the 6 button is pressed', ()=>{
+		let wrapper = shallow(<App/>);
+		wrapper.find('button[children="6"]')
+			.simulate('click');
+
+		expect(wrapper.find('#display>h2[children=6]').length).toBe(1)
+	})
+
+	it('Renders 7 when the 7 button is pressed', ()=>{
+		let wrapper = shallow(<App/>);
+		wrapper.find('button[children="7"]')
+			.simulate('click');
+
+		expect(wrapper.find('#display>h2[children=7]').length).toBe(1)
+	})
+
+	it('Renders 1 when the 8 button is pressed', ()=>{
+		let wrapper = shallow(<App/>);
+		wrapper.find('button[children="8"]')
+			.simulate('click');
+
+		expect(wrapper.find('#display>h2[children=8]').length).toBe(1)
+	})
+
+	it('Renders 9 when the 9 button is pressed', ()=>{
+		let wrapper = shallow(<App/>);
+		wrapper.find('button[children="9"]')
+			.simulate('click');
+
+		expect(wrapper.find('#display>h2[children=9]').length).toBe(1)
+	})
+
+	it('Renders 0 when the 0 button is pressed', ()=>{
+		let wrapper = shallow(<App/>);
+		wrapper.find('button[children="0"]')
+			.simulate('click');
+
+		expect(wrapper.find('#display>h2[children=0]').length).toBe(1)
+	})
+})
+
+describe("C button replaces currentNumber to 0", ()=>{
+
+	it('Clears 1 out after pressed by pressing C', ()=>{
+		let wrapper = shallow(<App/>)
+		wrapper.find('button[children="1"]')
+			.simulate('click');
+
+		wrapper.find('button[children="C"]')
+			.simulate('click');
+
+		expect(wrapper.find('#display>h2[children="0"]').length).toBe(1)
+	})
+
+	it('Clears 5 out after pressing C', ()=>{
+		let wrapper = shallow(<App/>)
+		wrapper.find('button[children="5"]')
+			.simulate('click');
+
+		wrapper.find('button[children="C"]')
+			.simulate('click');
+
+		expect(wrapper.find('#display>h2[children="0"]').length).toBe(1)
+	})
+})	
